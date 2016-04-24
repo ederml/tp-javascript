c9f67e016a94a2cd73b7717bff87be6fbe10bc5a
mbasprof <mbasprof@users.noreply.github.com>
2016-04-24 00:21:55 -0400

    Added files via upload
    Mise a jour de Marion 00:21 24-04-16 : affichage ds tableau du nom et du prix du forfait marche, ainsi que les validations du nom, prenom et courriel

--- script/form.js
+++ script/form.js
@@ -25,7 +25,7 @@ function soumettre(){
 
     //Branchement du gestionnaire d'événement
    // $("#form_resa").bind("submit", valider_form);
-    $("#btn_test").bind("click", valider_form);
+   //  $("#btn_test").bind("click", valider_form);
 
 
     valider_form();
@@ -37,7 +37,7 @@ function soumettre(){
         var formulaire_ok = true; //par défaut tous les champs sont valides
 
         //CODE DE VALIDATION
-        //console.log($("#choix_forfait"));
+
 
             // Un forfait doit être sélectionné  DEJA UN PAR DEFAUT
             console.log("Forfait sélectionné :", $('#choix_forfait option:selected').val());
@@ -49,9 +49,9 @@ function soumettre(){
 
 
 
-            // Le champ animaux doit s'afficher si le forfait admet les animaux
+            // Le champ animaux doit s'afficher si le forfait admet les animaux, sinon disabled
             //  Dans ce cas, un nombre d'animaux DEJA 1 PAR DEFAUT
-            console.log("Nombre de participants sélectionné :", $('#nb_animaux').val());
+            console.log("Nombre d'animaux sélectionné :", $('#nb_animaux').val());
             $('#nb_animaux').val();
 
             // Une date valide doit être sélectionnée :
@@ -98,7 +98,21 @@ function soumettre(){
         }); //END change prenom
 
             // Un courriel valide doit être saisi
+        $("#email").change(function () {
 
+            var val = $(this).val();
+            var input_text = new RegExp('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$');
+
+            if (!input_text.test(val)){
+                formulaire_ok = false; //on met à false
+                $(this).addClass("invalid");
+                $("#erreur_email").addClass("display");
+            }else if (0 == $("#email").val().length){
+                formulaire_ok = false;
+                $(this).addClass("invalid");
+                $("#erreur_email").addClass("display");
+            }
+        }); //END change email
 
 
             // Un numéro de tél. au format (xxx)xxx-xx-xx doit être saisi (chiffres)
@@ -118,23 +132,109 @@ function soumettre(){
     }
 
     console.groupEnd();
-    } //End valider_form
+    } //End soumettre()
 
 
-$("table tbody tr td:first").text($("#choix_forfait").val());
 
-   // Fonction de récupération des valeurs
-    $("#choix_forfait, [type='number']")
+// Forfait affiché par défaut dans le tableau récapitulatif
+var nom_forfait = monApp.produits[0].nom;
+$("table tbody tr td:first").text(nom_forfait);
+console.log(nom_forfait);
+
+//Prix du forfait qui s'affiche par défaut
+var prix_unitaire = monApp.produits[0].prix_basse_saison;
+$("table tbody tr:first-of-type td:last").text(prix_unitaire + " CAD");
+
+
+//Nombre de participants affiché par défaut dans le tableau récapitulatif
+var nb_participants = 0;
+$("table tbody tr td").eq(1).text(nb_participants);
+
+
+
+// $("table tbody tr:nth-of-type(2)  td").eq(1).text($("#nb_animaux").val());
+
+
+
+
+// Fonctions de récupération des valeurs
+    $("#choix_forfait")
         .change(function () {
+            var resultat;
 
             // un seul champ change à la fois donc c'est $(this) en jQuery et non pas each
             console.log("Valeur de ", this.name, "qui change", $(this).val());
-
+            resultat = $(this).val();
             //test insertion de la valeur d'un des forfaits dans le tableau apres selection
-            $("table tbody tr td:first").text($("#choix_forfait").val());
+            // $("table tbody tr td:first").text($("#choix_forfait").val());
+            console.log("resultat de la fonction", resultat);
+            afficher_nom_forfait(resultat); ///////JE N'ARRIVE PAS A PASSER LE RESULTAT DE LA FONCTION CHANGE EN PARAMETRE
+
+            return resultat;
+            // $("table tbody tr td").eq(1).text($("#nb_participants").val());
+            // $("table tbody tr:nth-of-type(2) td").eq(1).text($("#nb_animaux").val());
+
+        }); //END change#choix_forfait
 
 
-        }); //END change()
+// Fonctions de récupération des valeurs
+$("#nb_participants")
+    .change(function () {
+        var resultat;
 
+        // un seul champ change à la fois donc c'est $(this) en jQuery et non pas each
+        console.log("Valeur de ", this.name, "qui change", $(this).val());
+        resultat = $(this).val();
+        //test insertion de la valeur d'un des forfaits dans le tableau apres selection
+        // $("table tbody tr td:first").text($("#choix_forfait").val());
+        console.log("resultat de la fonction", resultat);
+        afficher_nom_forfait(resultat); ///////JE N'ARRIVE PAS A PASSER LE RESULTAT DE LA FONCTION CHANGE EN PARAMETRE
+
+        return resultat;
+        // $("table tbody tr td").eq(1).text($("#nb_participants").val());
+        // $("table tbody tr:nth-of-type(2) td").eq(1).text($("#nb_animaux").val());
+
+    }); //END change#choix_forfait
 
  // END soumettre()
+
+
+/***********FONCTION AFFICHER NOM_FORFAIT******/
+
+function afficher_nom_forfait(valeur_forfait){
+
+    //Initialisation
+    nom_forfait = monApp.produits[valeur_forfait].nom;
+    prix_unitaire = monApp.produits[valeur_forfait].prix_basse_saison;
+    console.log("nom du forfait a afficher ds le tableau : nom_forfait", nom_forfait);
+    $("table tbody tr td:first").text(nom_forfait);
+    $("table tbody tr:first-of-type td:last").text(prix_unitaire + " CAD");
+
+
+    return nom_forfait;
+
+    //Affichage
+
+
+}
+
+// afficher_prix_unitaire(2); ///////JE N'ARRIVE PAS A PASSER LE RESULTAT DE LA FONCTION CHANGE EN PARAMETRE
+
+/*****************FONCTION CALCULER_PRIX_TOTAL****************/
+
+function afficher_prix_unitaire(valeur_forfait){
+
+    //Initialisation du prix total
+    var prix_total ;
+    var prix_forfait ;
+    prix_forfait = monApp.produits[valeur_forfait].prix_basse_saison;
+    console.log("prix du forfait a afficher ds le tableau :", prix_forfait);
+    $("table tbody tr:first td:last").text(prix_forfait);
+
+    prix_total = prix_forfait * ($('#nb_participants').val());
+    $("table tfoot tr td:last-of-type").text(prix_total);
+
+    //Multiplier par le nombre de participants
+
+
+}
